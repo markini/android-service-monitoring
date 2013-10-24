c5dcb092c311d0c493c620fa774c470338da7748
markini <daniel.markart@gmail.com>
2013-10-24 17:21:18 +0200

    start test server

---
+++ TestEnvironment/Server/src/at/marki/Server/Server/SimpleJettyServer.java
@@ -0,0 +1,26 @@
+package at.marki.Server.Server;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+public class SimpleJettyServer {
+
+    public static void main(String[] args) throws Exception {
+
+        Server server = new Server(1433);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+//		context.addServlet(new ServletHolder(new ServletSendProjectData()), "/" + Servlet.GET_PROJECT_WITH_NUMMBER);
+//
+//		ServletHolder holder = new ServletHolder(new ServletMessageReceiver());
+//		MultipartConfigElement element = new MultipartConfigElement("data/tmp");
+//		holder.getRegistration().setMultipartConfig(element);
+//		context.addServlet(holder, "/" + Servlet.UPLOAD_MESSAGES);
+
+        server.start();
+        server.join();
+    }
+}
