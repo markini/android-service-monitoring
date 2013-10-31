5d15f1bc32633f69a340a34156c2bd3b2b81d5eb
markini <daniel.markart@gmail.com>
2013-10-31 02:09:14 +0100

    adding time to messages and starting new module TestMonitor

--- TestEnvironment/Server/src/at/marki/Server/Gui/ServerManagementGui.java
+++ TestEnvironment/Server/src/at/marki/Server/Gui/ServerManagementGui.java
@@ -6,7 +6,10 @@ import at.marki.Server.Servlet.GCMSend;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by marki on 27.10.13.
@@ -24,9 +27,8 @@ public class ServerManagementGui {
         buttonSendGcmMessage.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 String message = "";
-                if (editTextGcmMessage != null && editTextGcmMessage.getText() != null) {
-                    message = editTextGcmMessage.getText();
-                }
+
+                message = getMessage();
                 ArrayList<String> devices = new ArrayList<String>();
                 devices.add(Data.gcmId);
                 GCMSend.sendMessage(message, devices);
@@ -35,8 +37,8 @@ public class ServerManagementGui {
         });
         instance = this;
 
-        listLog.setModel(new DefaultListModel());
-        listMessages.setModel(new DefaultListModel());
+        listLog.setModel(new DefaultListModel<String>());
+        listMessages.setModel(new DefaultListModel<String>());
     }
 
     public void startGui() {
@@ -54,11 +56,14 @@ public class ServerManagementGui {
     public static String getMessage() {
         String message = "";
 
-        if (instance != null && instance.editTextGcmMessage != null) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM HH:mm", Locale.GERMANY);
+        Date resultTime = new Date(System.currentTimeMillis());
+
+        if (instance != null && instance.editTextGcmMessage != null && instance.editTextGcmMessage.getText() != null) {
             message = instance.editTextGcmMessage.getText();
         }
 
-        String time = "";
+        String time = sdf.format(resultTime);
         message = message + time;
         return message;
     }
